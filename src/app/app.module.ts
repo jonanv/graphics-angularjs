376001179d8b2ae27d0919ee548f466082e19013
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-03-30 22:26:21 -0500

    Se hace la instalacion de ng2-charts con npm install --save ng2-charts y se instala chars.js con npm install --save chart.js y se importa en el app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Imports
 import { AppRoutingModule } from './app-routing.module';
+import { ChartsModule } from 'ng2-charts';
+
+// Imports components
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
